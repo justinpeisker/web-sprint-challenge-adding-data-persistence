851db0b4679becbda508cf4e5eb3265a57239c7c
Justin Peisker <justinpeisker@gmail.com>
2022-01-20 17:48:55 -0800

    wip migrations scaffolded

---
+++ data/migrations/20220121013843_initial-migration.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('projects', table => {
+
+    })
+    .createTable('resources', table => {
+
+    })
+    .createTable('tasks', table => {
+
+    })
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+};
