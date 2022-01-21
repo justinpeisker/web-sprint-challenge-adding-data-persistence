37a99e8c81e2d296735b300ba64172b1be54b02a
Justin Peisker <justinpeisker@gmail.com>
2022-01-21 13:34:58 -0800

    finished!

--- api/task/model.js
+++ api/task/model.js
@@ -21,8 +21,19 @@ async function getAll(){
 //     leftJoin projects as p
 //         on t.project_name = p.user_id
 
-const getById = (id) => {
-    return db('tasks').where('task_id', id).first()
+const getById = async (id) => {
+    const rows = await db('tasks')
+        .where('task_id', id)
+        .first()
+
+    const result = {
+        task_description: rows.task_description,
+        task_notes: rows.task_notes,
+        task_completed: Boolean(rows.task_completed),
+        project_name: rows.project_name,
+        project_description: rows.project_description
+    }
+    return result;
   }
 
 function add(task) {
