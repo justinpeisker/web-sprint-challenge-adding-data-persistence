6597ffb08349ac7c973cbf96106d3f4d365e6947
Justin Peisker <justinpeisker@gmail.com>
2022-01-20 16:20:19 -0800

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,15 @@
-// build your `/api/projects` router here
+const router = require('express').Router()
+
+router.use('*', (req, res, next) => {
+    res.json({api: 'project router up'})
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        customMessage: 'oops',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
