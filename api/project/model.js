698be37e16ceecb93a04bf93ec3678565a1031d9
Justin Peisker <justinpeisker@gmail.com>
2022-01-21 12:44:58 -0800

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -2,8 +2,14 @@ const db = require('../../data/dbConfig')
 
 async function getAll(){
     const rows = await db('projects')
-
-    return rows
+    const result = rows.map(item => {
+        return{
+        project_name: item.project_name,
+        project_description: item.project_description,
+        project_completed: item.project_completed
+        }
+    })
+    return result
 }
 
 const getById = (id) => {
